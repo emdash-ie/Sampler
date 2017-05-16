e00f946e4b36fd0deaafdfaebb70e1841a4fdb93
Noel Bourke <noelfbourke@gmail.com>
2017-05-16 13:16:01 +0100

    Removing password again (missed one)

--- load_settings.py
+++ load_settings.py
@@ -39,7 +39,7 @@ if len(form_data) != 0:
 
 	elif display:
 		try:
-			connection = db.connect('cs1dev.ucc.ie', 'nfb2', '***REMOVED***', '2019_nfb2')
+			connection = db.connect('cs1dev.ucc.ie', 'nfb2', '', '2019_nfb2')
 			cursor = connection.cursor(db.cursors.DictCursor)
 			cursor.execute('''SELECT name, id FROM presets''')
 			count = 0
